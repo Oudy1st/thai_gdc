0b52828e50fd8cf6fdc76713891f8bb6648ae24d
Detchat <Oudy1st@gmail.com>
2022-04-11 07:46:53 +0700

    try

--- ckanext/thai_gdc/controllers/user.py
+++ ckanext/thai_gdc/controllers/user.py
@@ -7,6 +7,8 @@ import ckan.model as model
 import logging
 from ckan.common import g
 
+import ckan.lib.base as base
+
 from ckan.plugins.toolkit import (
     _, c, h, BaseController, check_access, NotAuthorized, abort, render,
     redirect_to, request,
@@ -53,5 +55,99 @@ class UserManageController(plugins.toolkit.BaseController):
 class OICLoginController(plugins.toolkit.BaseController):
 
     def login(self):
+        #extra_vars = {}
+        #return render('home/oiclogin.html', extra_vars=extra_vars)
+        group_type = 'organization'
+        is_organization = True
         extra_vars = {}
-        return render('home/oiclogin.html', extra_vars=extra_vars)
+        page = h.get_page_number(request.params) or 1
+        items_per_page = int(config.get(u'ckan.datasets_per_page', 20))
+
+        context = {
+            u'model': model,
+            u'session': model.Session,
+            u'user': g.user,
+            u'for_view': True,
+            u'with_private': False
+        }
+
+        try:
+            _check_access(u'site_read', context)
+            _check_access(u'group_list', context)
+        except NotAuthorized:
+            base.abort(403, _(u'Not authorized to see this page'))
+
+        q = request.params.get(u'q', u'')
+        if q=='':
+            extra_vars["page"] = h.Page([], 0)
+            extra_vars["group_type"] = group_type
+            return base.render(
+                _get_group_template(u'index_template', group_type), extra_vars)
+        sort_by = request.params.get(u'sort')
+
+        # TODO: Remove
+        # ckan 2.9: Adding variables that were removed from c object for
+        # compatibility with templates in existing extensions
+        g.q = q
+        g.sort_by_selected = sort_by
+
+        extra_vars["q"] = q
+        extra_vars["sort_by_selected"] = sort_by
+
+        # pass user info to context as needed to view private datasets of
+        # orgs correctly
+        if g.userobj:
+            context['user_id'] = g.userobj.id
+            context['user_is_admin'] = g.userobj.sysadmin
+
+        try:
+            data_dict_global_results = {
+                u'all_fields': False,
+                u'q': q,
+                u'sort': sort_by,
+                u'type': group_type or u'group',
+            }
+            global_results = _action(u'group_list')(context,
+                                                    data_dict_global_results)
+        except ValidationError as e:
+            if e.error_dict and e.error_dict.get(u'message'):
+                msg = e.error_dict['message']
+            else:
+                msg = str(e)
+            h.flash_error(msg)
+            extra_vars["page"] = h.Page([], 0)
+            extra_vars["group_type"] = group_type
+            return base.render(
+                _get_group_template(u'index_template', group_type), extra_vars)
+
+        data_dict_page_results = {
+            u'all_fields': True,
+            u'q': q,
+            u'sort': sort_by,
+            u'type': group_type or u'group',
+            u'limit': items_per_page,
+            u'offset': items_per_page * (page - 1),
+            u'include_extras': True
+        }
+        page_results = _action(u'group_list')(context, data_dict_page_results)
+
+        extra_vars["page"] = h.Page(
+            collection=global_results,
+            page=page,
+            url=h.pager_url,
+            items_per_page=items_per_page, )
+
+        extra_vars["page"].items = page_results
+        extra_vars["group_type"] = group_type
+
+        # TODO: Remove
+        # ckan 2.9: Adding variables that were removed from c object for
+        # compatibility with templates in existing extensions
+        g.page = extra_vars["page"]
+        return base.render(
+            _get_group_template(u'index_template', group_type), extra_vars)
+
+
+    def index(self):
+        extra_vars = {}
+        return base.render('home/oiclogin.html', extra_vars=extra_vars)
--- ckanext/thai_gdc/plugin.py
+++ ckanext/thai_gdc/plugin.py
@@ -164,7 +164,7 @@ class Thai_GDCPlugin(plugins.SingletonPlugin, DefaultTranslation, toolkit.Defaul
             'oic_login',
             '/oiclogin/',
             action='index',
-            controller='ckanext.thai_gdc.controllers.organization:OrganizationCustomController'
+            controller='ckanext.thai_gdc.controllers.user:OICLoginController'
         )
 
         return map
