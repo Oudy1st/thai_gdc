0d9ac4242033a37e3c0b7b21763b7b2a1ce281b7
Detchat <Oudy1st@gmail.com>
2022-04-13 21:09:53 +0700

    Update user.py

--- ckanext/thai_gdc/controllers/user.py
+++ ckanext/thai_gdc/controllers/user.py
@@ -70,6 +70,42 @@ class OICLoginController(plugins.toolkit.BaseController):
         for n in xrange(8):
             out += str(uuid.uuid4())
         return out
+        
+    def is_sysadmin(self, user_data):
+        return user_data.name == 'oudy'
+
+    def checkUser(self, user_data):
+        try:
+            user = plugins.toolkit.get_action('user_show')(
+                {'return_minimal': True,
+                 'keep_sensitive_data': True,
+                 'keep_email': True},
+                {'id': user_data.name}
+            )
+        except plugins.toolkit.ObjectNotFound:
+            pass
+            user = None
+        if user:
+            # update the user in ckan only if ckan data is not matching drupal data
+            update = False
+            if user_data.mail != user['email']:
+                update = True
+            if self.is_sysadmin(user_data) != user['sysadmin']:
+                update = True
+            if update:
+                user['email'] = user_data.mail
+                user['sysadmin'] = self.is_sysadmin(user_data)
+                user['id'] = user_data.name
+                user = plugins.toolkit.get_action('user_update')({'ignore_auth': True}, user)
+        else:
+            user = {'email': user_data.mail,
+                    'name': user_data.name,
+                    'password': self.make_password(),
+                    'sysadmin': self.is_sysadmin(user_data)}
+            user = plugins.toolkit.get_action('user_create')({'ignore_auth': True}, user)
+        plugins.toolkit.c.user = user['name']
+
+        return user
 
     def index(self):
         extra_vars = {}
@@ -81,13 +117,16 @@ class OICLoginController(plugins.toolkit.BaseController):
             password = data['password']
             extra_vars = {'data': data, 'errors': {}, 'username': data['username']}
 
-            user = {'email': 'oudy2nd@gmail.com',
+            user = {'email': 'oudy1st@gmail.com',
                     'name': data['username'],
                     'password': self.make_password(),
-                    'sysadmin': True }
-            user = plugins.toolkit.get_action('user_create')({'ignore_auth': True}, user)
-            session['oic-user'] = user['name']
-            session.save()
+                    'sysadmin': self.is_sysadmin(data['username']) }
+                    
+            cUser = self.checkUser(user)
+
+            if cUser != None:
+                session['oic-user'] = cUser['name']
+                session.save()
 
             return toolkit.redirect_to('user.logged_in')
 
